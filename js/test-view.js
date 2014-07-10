9cc7a0497be651bf6c9e59f3b82574a550f186e9
Doug <>
2014-07-10 14:33:41 -0600

    updated test view

--- js/test-view.js
+++ js/test-view.js
@@ -1,8 +1,26 @@
+var TestView = (function(){
+  function create(element){
+    bind(element);
+    element.gatherSelectors();
+  }
+  function bind(element){
+    element.renderShadow = renderShadow.bind(element);
+    element.gatherSelectors = gatherSelectors.bind(element);
+  }
+  function gatherSelectors(){
+    
+  }
+  return {
+    create : create
+  };
+})();
+
 var testViewProto = Object.create(HTMLElement.prototype);
 Object.defineProperty(testViewProto, "test", {
   writable : true
 });
 testViewProto.createdCallback = function(){
+  var self = this;
   var template = document.getElementById("test-tmpl");
   var clone = document.importNode(template.content, true);
   var shadowRoot = this.createShadowRoot();
@@ -13,9 +31,9 @@ testViewProto.createdCallback = function(){
     this.classList.remove("failure");
     TestRunner.runTest(this.test).then(function(){
       this.classList.add("success");
-    }).catch(function(){
+    }.bind(this)).catch(function(){
       this.classList.add("failure");
-    });
+    }.bind(this));
   }.bind(this));
 };
 
