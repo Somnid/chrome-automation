e2c2ffd3bc9bf73cd5a6a7c14731b595820f21af
Doug <>
2014-07-10 13:39:31 -0600

    working around CSP for js loading

--- tests/search-lift-tickets-test.js
+++ tests/search-lift-tickets-test.js
@@ -1,4 +1,4 @@
-Actions.navigate(null, "http://local.vail.com/sf/slt")
+return Actions.navigate(null, "http://local.vail.com/sf/slt")
 .then(function(results){
   return Actions.updateValue(results.tab, "[id$='txtArrivalDate']", "12/1/2014");
 })
@@ -12,11 +12,5 @@ Actions.navigate(null, "http://local.vail.com/sf/slt")
   return Actions.clickElement(results.tab, "[id$='btnFindTickets']");
 })
 .then(function(results){
-  return Actions.waitUntilUrl(results.tab, ".*/sf/lsr");
-})
-.then(function(results){
-  return Actions.printToPageConsole("Success!")
-})
-.catch(function(error){
-  return console.log(error);
+  return Actions.waitUntilUrl(results.tab, ".*/sf/ltr*");
 });
