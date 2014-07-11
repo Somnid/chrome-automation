7ab48cf3d832755fe4e8973a283e490528a26f6f
Doug <>
2014-07-11 16:43:43 -0600

    updates

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
 	var btnCurrent = document.getElementById("current");
-	  
-	var configPath = chrome.extension.getURL("/config/test-config.json");
-	Ajax.promiseRequest({ url : configPath }).then(function(content){
-    var testConfig = JSON.parse(content);
-    TestUiView.create({ model : testConfig });
+	
+	chrome.storage.local.get("config-location", function(values){
+      var configPath = values["config-location"] || chrome.extension.getURL("/config/test-config.json");
+      Ajax.promiseRequest({ url : configPath }).then(function(content){
+        var testConfig = JSON.parse(content);
+        TestUiView.create({ model : testConfig });
+      });
 	});
 });
--- js/test-runner.js
+++ js/test-runner.js
@@ -3,7 +3,10 @@ var TestRunner = (function(){
     arrayOfTests.forEach(runTest);
   }
   function runTest(test){
-    var filePath = chrome.extension.getURL(test.file);
+    var filePath = test.file 
+    if(test.isLocal){ //hack for now
+      filePath = chrome.extension.getURL(test.file);
+    }
     return Ajax.promiseRequest({ url : filePath }).then(function(fileContents){
       var func = new Function("params", "Actions", fileContents);
       return func(Actions);
