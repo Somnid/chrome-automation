f207b01fd00654cb5afb3ac42ab743448d100307
Doug <>
2015-09-29 16:50:04 -0600

    new simple syntax!

--- js/libs/utilities.js
+++ js/libs/utilities.js
@@ -28,11 +28,53 @@ var Util = (function(){
 	function isAbsolutePath(path){
 	  return path.indexOf(":") != -1;
 	}
+	
+	function isWhitespace(char){
+    var whitespace = [
+      String.fromCharCode(13), //carriage return
+      String.fromCharCode(10), //new line
+      String.fromCharCode(32), //space
+      String.fromCharCode(9)   //tab
+    ];
+    return whitespace.indexOf(char) != -1;
+  }
+	
+	function splitWhitespace(text){
+	  var split = [];
+	  var buffer = "";
+	  var quoted = false;
+	  var readWhitespace = false;
+	  for(var i = 0; i < text.length; i++){
+	    if(isWhitespace(text[i]) && !quoted && !readWhitespace){
+	      split.push(buffer);
+	      buffer = "";
+	      readWhitespace = true;
+	    }else if(isWhitespace(text[i]) && !quoted && readWhitespace){
+	      continue;
+	    }else if(text[i] == "\"" && !quoted){
+	      quoted = true;
+	      readWhitespace = false;
+	    }else if(text[i] == "\"" && quoted){
+	      quoted = false;
+	      readWhitespace = false;
+	    }else{
+	      buffer += text[i];
+	      readWhitespace = false;
+	    }
+	  }
+	  if(buffer){
+	    split.push(buffer);
+	  }
+	  
+	  return split;
+	}
 
   return {
     stringToDataUri : stringToDataUri,
     appendScript : appendScript,
     getParentDirectory : getParentDirectory,
-    isAbsolutePath : isAbsolutePath
+    isAbsolutePath : isAbsolutePath,
+    isWhitespace : isWhitespace,
+    splitWhitespace : splitWhitespace
   };
 })();
