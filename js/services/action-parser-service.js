9396fa41e08eec339a612f040de15c45880eafc8
Doug <>
2015-09-30 17:47:36 -0600

    working generators

--- js/services/action-parser-service.js
+++ js/services/action-parser-service.js
@@ -1,7 +1,8 @@
 var ActionParserService = (function(){
   
   var defaults = {
-    actions : null //required
+    actions : null, //required
+    generator : null //required
   };
   
   function create(options){
@@ -14,6 +15,8 @@ var ActionParserService = (function(){
   function bind(actionParserService){
     actionParserService.parse = parse.bind(actionParserService);
     actionParserService.parseAsAction = parseAsAction.bind(actionParserService);
+    actionParserService.parseArgument = parseArgument.bind(actionParserService);
+    actionParserService.parseGenerator = parseGenerator.bind(actionParserService);
   }
 
   function parse(script){
@@ -32,11 +35,27 @@ var ActionParserService = (function(){
   function parseAsAction(line){
     var parts = Util.splitWhitespace(line);
     var action = this.options.actions.actionMap[parts[0]];
-    var args = parts.slice(1).map(x => x.trim());
+    var args = parts.slice(1).map(x => this.parseArgument(x));
     return {
       action : action,
       args : args
     };
+  }
+  
+  function parseArgument(arg){
+    arg = arg.trim();
+    arg = Util.transformToken(arg, /\{.*\}/, x => this.parseGenerator(x));
+    return arg;
+  }
+  
+  function parseGenerator(generatorText){
+    var text = generatorText.replace(/[\{\}]/g, "");
+    var split = text.split(",").map(x => x.trim());
+    var generator = this.options.generator[split[0]];
+    if(!generator){
+      throw `Could not find generator for "${split[0]}"`;
+    }
+    return generator.apply(null, split.slice(1));
   }
   
   return {
