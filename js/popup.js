d096155eb8f7201dfeb101408be01baa38d1534e
Doug <>
2016-02-29 14:36:58 -0700

    update

--- js/popup.js
+++ js/popup.js
@@ -1,20 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
 	var btnCurrent = document.getElementById("current");
 
-	chrome.storage.local.get("config-location", function(values){
-      var configPath = values["config-location"] || chrome.extension.getURL("/config/test-config.json");
-      Ajax.promiseRequest({ url : configPath })
-        .then(function(content){
-          var testConfig = JSON.parse(content);
-          TestUiView.create({
-            model : testConfig,
-            basePath : Util.getParentDirectory(configPath)
-          });
-        },
-        function(error){
-          var errorElement = document.getElementById("error");
-          errorElement.innerText = "Error getting config from location: " + configPath;
-          errorElement.classList.add("show");
-        });
-	});
+	chromep.storage.local.get("tests")
+	  .then((values) => {
+      var tests = values["tests"] || [];
+      TestUiView.create({
+        model : tests
+      });
+    });
 });
